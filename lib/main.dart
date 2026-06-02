e5d65523c30dc4f345522037e4f5a974299cd6ba
farhanamiri <jamalazad525@gmail.com>
2026-06-02 12:17:19 +0500

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Mubarak Shahi')),
+        body: Center(
+          child: Text(
+            'App چل گئی ✅',
+            style: TextStyle(fontSize: 24),
+          ),
+        ),
+      ),
+    );
+  }
+}
